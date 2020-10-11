92dc83422a6d017fe88aaa647524d5433e706c24
Jenni Schubert <jenni.schubert@gmail.com>
2020-10-11 09:51:22 -0500

    add display title

--- src/Components/EqList/EqList.jsx
+++ src/Components/EqList/EqList.jsx
@@ -5,6 +5,7 @@ class EqList extends Component {
   render() {
     return (
       <div className="list">
+          <h3>Recent Calculations</h3>
         <ul>
             {this.props.eqHistory.map((eq, i) => {
                 return <li key={i}>{eq}</li>
